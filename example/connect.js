be182dd03c1f69d39b41bf5b7920c13d1903f530
cinlk <wowpandan@gmail.com>
2022-03-22 11:09:05 +0800

    add usecase (#56)
    * add usecase
    
    Merge darwinia-js-usecase
    
    * fix: import darwinia types
    
    * refactor: typesBundle
    
    Co-authored-by: cinlk <wayne.liang@itering.io>

---
+++ example/connect.js
@@ -0,0 +1,24 @@
+// external imports
+/* eslint-disable */
+
+const { typesBundleForPolkadotApps } = require("@darwinia/types/mix");
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+
+const darwiniaTypesBundle = typesBundleForPolkadotApps;
+
+const wsProvider = new WsProvider("wss://rpc.darwinia.network");
+
+ApiPromise.create({ provider: wsProvider, typesBundle: darwiniaTypesBundle })
+  .then((api) => {
+    api.query.system
+      .account("<your wallet address>")
+      .then(({ nonce, data }) => {
+        console.log(`balance of ${data.free} and a nonce of ${nonce}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
