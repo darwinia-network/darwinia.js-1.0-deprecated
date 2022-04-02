6dcd1ceef245e214f068d86546918bbd87620fc2
cinlk <wayne.liang@itering.io>
2022-04-02 16:34:31 +0800

    chore: add return type

--- packages/api-derive/src/staking/account.ts
+++ packages/api-derive/src/staking/account.ts
@@ -3,14 +3,15 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { StakingLedgerT as StakingLedger } from '../../../types/src';
-import { DeriveApi, DeriveUnlocking } from '@polkadot/api-derive/types';
+import { DeriveApi, DeriveUnlocking, DeriveStakingKeys } from '@polkadot/api-derive/types';
 import { firstMemo, memo } from '@polkadot/api-derive/util';
 import { ApiInterfaceRx } from '@polkadot/api/types';
 import type { Balance, BlockNumber } from '@polkadot/types/interfaces';
 import { Moment } from '@polkadot/types/interfaces';
 import { BN } from '@polkadot/util';
 import { isUndefined } from 'lodash';
-import { combineLatest, map } from 'rxjs';
+import { Memoized } from '@polkadot/util/types';
+import { combineLatest, map, Observable } from 'rxjs';
 import { DeriveStakingAccount, DeriveStakingQuery, StakingLock } from './types';
 
 const QUERY_OPTS = {
@@ -89,8 +90,7 @@ function parseResult (api: DeriveApi, best: BlockNumber, now: Moment, query: Der
 /**
  * @description From a list of stashes, fill in all the relevant staking details
  */
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function accounts (instanceId: string, api: DeriveApi) {
+export function accounts (instanceId: string, api: DeriveApi) : Memoized<(accountIds:(Uint8Array | string)[]) => Observable<DeriveStakingAccount & DeriveStakingKeys>> {
   return memo(instanceId, (accountIds: (Uint8Array | string)[]) => {
     const keysObs = api.derive.staking.keysMulti(accountIds);
     const queryObs = api.derive.staking.queryMulti(accountIds, QUERY_OPTS);
