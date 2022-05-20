1c30166d900e72ade5bd8e9d2c87dbf192abbfa7
wayne.liang <wayne.liang@itering.io>
2022-05-20 09:28:49 +0800

    export darwinia api derived

--- packages/api-derive/src/bundle.ts
+++ packages/api-derive/src/bundle.ts
@@ -7,12 +7,17 @@ import type { DeriveUsableAccount } from './accounts/types';
 import type { PowerOf } from './darwiniaStaking/types';
 import type { DeriveMarketFee } from './feeMarket/types';
 
+import { DeriveCustom } from '@polkadot/api/types';
 import { AccountId, LaneId, MessageNonce } from '@polkadot/types/interfaces';
 import { AnyNumber, ITuple } from '@polkadot/types-codec/types';
 
+import * as accounts from './accounts';
+import * as darwiniaStaking from './darwiniaStaking';
+import * as feeMarket from './feeMarket';
+
 declare module '@polkadot/api-derive/derive' {
   export interface ExactDerive {
-    // our derive section definition
+    // declare darwinia api derive definition
     fee: { markert: ReturnType<() => () => Observable<DeriveMarketFee | undefined>>; inProcessOrder: ReturnType<() => () => Observable<ITuple<[LaneId, MessageNonce]>[]>> };
     usableBalance: {
       balance: ReturnType<() => (tokenType: u8 | AnyNumber | Uint8Array, accountId: AccountId | string | Uint8Array) => Observable<DeriveUsableAccount>>;
@@ -22,3 +27,17 @@ declare module '@polkadot/api-derive/derive' {
     };
   }
 }
+
+// registry related api derive function
+export const darwiniaDerive = {
+  darwiniaStaking: {
+    powerOf: darwiniaStaking.powerOf
+  },
+  fee: {
+    inProcessOrder: feeMarket.inProcessOrders,
+    markert: feeMarket.marketFee
+  },
+  usableBalance: {
+    balance: accounts.usableAccount
+  }
+} as DeriveCustom;
