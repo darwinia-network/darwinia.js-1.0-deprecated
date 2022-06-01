3e06a8f44dd45cb0ffb20430523d9e376e694452
cinlk <wowpandan@gmail.com>
2022-06-01 15:12:37 +0800

    fix: prior crab address type (#78)
    Co-authored-by: wayne.liang <wayne.liang@itering.io>

--- packages/types-known/src/spec/index.ts
+++ packages/types-known/src/spec/index.ts
@@ -3,14 +3,14 @@
 
 import { OverrideVersionedType } from '@polkadot/types/types';
 
+import crab from './crab';
 import darwinia from './darwinia';
-import darwiniaCrab from './darwinia-crab';
 import pangolin from './pangolin';
 import pangoro from './pangoro';
 
 // Type overrides for specific spec types & versions as given in runtimeVersion
 const typesSpec: Record<string, OverrideVersionedType[]> = {
-  Crab: darwiniaCrab,
+  Crab: crab,
   Darwinia: darwinia,
   Pangolin: pangolin,
   Pangoro: pangoro
