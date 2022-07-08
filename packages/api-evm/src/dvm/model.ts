70bb87f9d20b54fa7652c51cfe3aad4e54ca24f0
wayne.liang <wayne.liang@itering.io>
2022-07-08 13:14:52 +0800

    change DispatchPrecompileAddress

--- packages/api-evm/src/dvm/model.ts
+++ packages/api-evm/src/dvm/model.ts
@@ -5,17 +5,24 @@ import type { HexString } from '@polkadot/util/types';
 
 import crab from '@darwinia/types-support/src/metadata/static-crab';
 import pangolin from '@darwinia/types-support/src/metadata/static-pangolin';
+import pangoro from '@darwinia/types-support/src/metadata/static-pangoro';
 
 import { EvmConfig } from './api';
 
 export const PangolinCallIndex: EvmConfig = {
   balancesTransferIndex: [4, 0],
-  dispatchContractAddress: '0x0000000000000000000000000000000000000019',
+  dispatchContractAddress: '0x0000000000000000000000000000000000000401',
   metadata: pangolin as HexString
+};
+
+export const PangoroCallIndex: EvmConfig = {
+  balancesTransferIndex: [4, 0],
+  dispatchContractAddress: '0x0000000000000000000000000000000000000401',
+  metadata: pangoro as HexString
 };
 
 export const CrabCallIndex: EvmConfig = {
   balancesTransferIndex: [23, 0],
-  dispatchContractAddress: '0x0000000000000000000000000000000000000019',
+  dispatchContractAddress: '0x0000000000000000000000000000000000000401',
   metadata: crab as HexString
 };
